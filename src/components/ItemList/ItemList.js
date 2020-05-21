4da5f02e31c240fb069ebbd06db9b17c656f7680
Kseniya Molkova <52568987+Costyanica@users.noreply.github.com>
2020-05-21 21:43:23 +0300

    перебор в React

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import Item from '../Item/Items';
 
-const ItemList = props => { 
-    const todoItem = [ 
-    {id: 1, affair: 'Написать новое приложение'},
-    {id: 2, affair: 'Прописать props-ы'},
-    {id: 3, affair: 'Сделать все дела'},
-    {id: 4, affair: 'Постричься'}
-];
-
-return (
-    <ul>
-        {todoItem.map(item =>(
-            <li key={item.id}>{item.affair}</li>
-        ))}
-    </ul>
-);
-};
+const ItemList = ({ items }) => (<ul>
+    {items.map(item => <li key={item.id}>
+        <Item value={item.value} /></li>)}
+</ul>);
 
 export default ItemList;
