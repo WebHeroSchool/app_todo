5565dbb346de5f6dd049a115a256007cf4ed88d2
Kseniya Molkova <52568987+Costyanica@users.noreply.github.com>
2020-06-18 22:24:49 +0300

    Material-UI

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<input></input>);
+const InputItem = () => (<div>
+    <TextField
+        label="Добавить задание"
+        variant="outlined"
+        id="custom-css-outlined-input"
+    />
+</div>);
 
 export default InputItem;
