208c7b60c7dc636da32775bf0e2877956d05f8b9
Kseniya Molkova <52568987+Costyanica@users.noreply.github.com>
2020-07-11 09:33:14 +0300

    Компонент -класс

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,35 +4,38 @@ import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
-const App = () => {
-    const items = [
-        {   id: 1, 
-            value: 'Написать новое приложение', 
-            isDone:true
-        },
-        {   id: 2, 
-            value: 'Прописать props-ы', 
-            isDone:false
-        },
-        {   id: 3,
-            value: 'Сделать все дела',
-            isDone:true
-        },
-        {   id: 4,
-            value: 'Постричься',
-            isDone:false
-        }
-    ];
-
- return (
-    <div className = {styles.wrap}>
-        <h1 className= {styles.title}>Список важных дел:</h1>
-        <div className = {styles.container}>
-            <InputItem />
-            <ItemList items={items} />
-            <Footer count={2} />
-        </div>
-    </div>);
+class App extends React.Component {
+    render () {
+        const items = [
+            {   id: 1, 
+                value: 'Написать новое приложение', 
+                isDone:true
+            },
+            {   id: 2, 
+                value: 'Прописать props-ы', 
+                isDone:false
+            },
+            {   id: 3,
+                value: 'Сделать все дела',
+                isDone:true
+            },
+            {   id: 4,
+                value: 'Постричься',
+                isDone:false
+            }
+        ];
+    
+     return (
+            <div className = {styles.wrap}>
+                <h1 className= {styles.title}>Список важных дел:</h1>
+                <div className = {styles.container}>
+                    <InputItem />
+                    <ItemList items={items} />
+                    <Footer count={2} />
+                </div>
+            </div>
+         );
+    }
 }
 
 export default App;
